ea8ae1069b2ac70dbebb04b85abb4d3b86e779e4
N4yth <nathan.dupuis31@gmail.com>
2025-06-18 08:55:18 +0200

    style : change the way to check the value when init an instance

--- app/models/user.py
+++ app/models/user.py
@@ -4,23 +4,10 @@ import re
 class User(BaseModel):
     def __init__(self, first_name, last_name, email, is_admin=False):
         super().__init__()
-        if(len(first_name) > 50):
-            raise ValueError ("First name to long")
-        else:
-            self._first_name = first_name
-        if(len(last_name) > 50):
-            raise ValueError ("Last name to long")
-        else:
-            self._last_name = last_name
-        if(re.fullmatch(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b', email)):
-            self._email = email
-        else:
-            raise ValueError ("Email is not conforme to standard")
+        self.first_name = (first_name)
+        self.last_name = (last_name)
+        self.email = (email)
         self.is_admin = is_admin
-        self.places = []
-
-    def add_place(self, place):
-        self.places.append(place)
 
     @property
     def email(self):
