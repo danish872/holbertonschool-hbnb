5468c7485e6469f4c52972ef6113f74db47f86e8
MinutePapillon <christophebarrere@gmail.com>
2025-06-19 14:13:46 +0200

    fix: create error message for update

--- app/models/amenity.py
+++ app/models/amenity.py
@@ -4,3 +4,14 @@ class Amenity(BaseModel):
     def __init__(self, name):
         super().__init__()
         self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if(len(name) <= 50):
+            self._name = name
+        else:
+            raise ValueError ("name is too long")
