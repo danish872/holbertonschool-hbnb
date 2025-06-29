0515c315861ee9e973e0b0849d753fcc9c993216
N4yth <nathan.dupuis31@gmail.com>
2025-06-29 13:07:38 +0200

    feat : Update method to receive a configuration

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_restx import Api
+import sqlalchemy as db
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
 
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
+    app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
     # Enregistrement du namespace pour les amenities
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
