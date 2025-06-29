99d148343f59bd8a6d0a0691dec4d20056e02ebd
N4yth <nathan.dupuis31@gmail.com>
2025-06-29 13:18:12 +0200

    feat : update the user models to accept password and create 2 module to hash and verifiy the given password

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -3,12 +3,21 @@ from datetime import datetime
 import re
 
 class User(BaseModel):
-    def __init__(self, first_name, last_name, email, is_admin=False):
+    def __init__(self, first_name, last_name, email, is_admin=False, password):
         super().__init__()
-        self.first_name = (first_name)
-        self.last_name = (last_name)
-        self.email = (email)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
         self.is_admin = is_admin
+        self.password = password
+
+    def hash_password(self, password):
+        """Hashes the password before storing it."""
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+        """Verifies if the provided password matches the hashed password."""
+        return bcrypt.check_password_hash(self.password, password)
 
     @property
     def email(self):
