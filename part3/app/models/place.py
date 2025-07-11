c59475e951231e135cd6f527de956aee3452216b
N4yth <nathan.dupuis31@gmail.com>
2025-07-11 14:21:27 +0200

    feat : transform setted and getter into validates

--- part3/app/models/place.py
+++ part3/app/models/place.py
@@ -1,19 +1,19 @@
 from app import db
-from sqlalchemy.orm import relationship
-from models.base_model import BaseModel
+from sqlalchemy.orm import relationship, validates
+from app.models.base_model import BaseModel
 
 # Association table many-to-many
 place_amenity = db.Table('place_amenity',
     db.Column('place_id', db.String(36), db.ForeignKey('place.id'), primary_key=True),
-    db.Column('amenity_id', db.String(36), db.ForeignKey('amenity.id'), primary_key=True)
+    db.Column('amenity_id', db.String(36), db.ForeignKey('amenities.id'), primary_key=True)
 )
 
-class Place(BaseModel, db.Model):
+class Place(BaseModel):
     __tablename__ = 'place'
 
     title = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text)
-    price = db.Column(db.Numeric(10, 2), nullable=False)
+    price = db.Column(db.Float, nullable=False)
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
     owner_id = db.Column(db.String(36), db.ForeignKey('user.id'), nullable=False)
@@ -21,69 +21,31 @@ class Place(BaseModel, db.Model):
     reviews = relationship('Review', backref='place', lazy=True)
     amenities = relationship('Amenity', secondary=place_amenity, backref=db.backref('places', lazy='dynamic'))
 
-    def add_review(self, review):
-        self.reviews.append(review)
-
-    def add_amenity(self, amenity):
-        self.amenities.append(amenity)
-
-    @property
-    def owner(self):
-        return self._owner
-
-    @owner.setter
-    def owner(self, owner):
-        if (isinstance(owner, User)):
-            self._owner = owner
-            self.save()
-        else:
-            raise ValueError ("owner not found")
-
-    @property
-    def title(self):
-        return self._title
-
-    @title.setter
-    def title(self, title):
-        if(len(title) < 101):
-            self._title = title
-            self.save()
+    @validates("title")
+    def validate_title(self, key, title):
+        if(len(title) < 101 and title != ""):
+            return title
         else:
             raise ValueError ("Title is not conforme to standar")
 
-    @property
-    def price(self):
-        return self._price
-
-    @price.setter
-    def price(self, price):
+    @validates("price")
+    def validate_price(self, key, price):
         if(price >= 0):
-            self._price = price
-            self.save()
+            return price
         else:
             raise ValueError ("price must be positif")
     
-    @property
-    def latitude(self):
-        return self._latitude
-
-    @latitude.setter
-    def latitude(self, latitude):
+    @validates("latitude")
+    def validate_latitude(self, key, latitude):
         if(latitude >= -90 and latitude <= 90):
-            self._latitude = latitude
-            self.save()
+            return latitude
         else:
             raise ValueError ("latitude must be within the range of -90.0 to 90.0")
 
-    @property
-    def longitude(self):
-        return self._longitude
-
-    @longitude.setter
-    def longitude(self, longitude):
+    @validates("longitude")
+    def validate_longitude(self, key, longitude):
         if(longitude >= -180 and longitude <= 180):
-            self._longitude = longitude
-            self.save()
+            return longitude
         else:
             raise ValueError ("longitude must be within the range of -180.0 to 180.0")
 
@@ -96,5 +58,6 @@ class Place(BaseModel, db.Model):
             "latitude": self.latitude,
             "longitude": self.longitude,
             "owner": self.owner.to_dict(),
-            "amenities": [element.to_dict() for element in self.amenities]
+            "amenities": [element.to_dict() for element in self.amenities],
+            "reviews": [element.to_dict() for element in self.reviews]
         }
