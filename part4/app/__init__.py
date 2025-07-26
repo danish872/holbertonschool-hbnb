807b4d792e97e617d6daaeabdb8c5342480ab95a
N4yth <nathan.dupuis31@gmail.com>
2025-07-27 01:03:42 +0200

    feat : add CORS module to handle Cross Origin error from the front

--- part4/app/__init__.py
+++ part4/app/__init__.py
@@ -3,6 +3,7 @@ from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
@@ -17,6 +18,7 @@ from app.api.v1.amenities import api as amenities_ns
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
+    CORS(app, supports_credentials=True)
     app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
